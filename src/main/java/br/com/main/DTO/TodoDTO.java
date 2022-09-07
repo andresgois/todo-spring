0fae365000815d89896115f5368e191b81e485ff
Andre Gois <andresousalee@gmail.com>
2022-09-07 10:11:26 -0300

    :sparkles: feat: add docker compose

---
+++ src/main/java/br/com/main/DTO/TodoDTO.java
@@ -0,0 +1,14 @@
+package br.com.main.DTO;
+
+import java.time.LocalDateTime;
+
+public class TodoDTO {
+    
+private String description;
+    
+    private Boolean done;
+
+    private LocalDateTime createdDate;
+    
+    private LocalDateTime doneDate;
+}
