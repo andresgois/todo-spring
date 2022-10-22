dfd6fca34ceb4b75a8052b14a6c0d68f941bce2d
Andre Gois <francisco.gois@pdcase.com.br>
2022-10-22 16:44:43 -0300

    :sparkles: feat: criacao dto de erro padrao

---
+++ src/main/java/br/com/main/dto/StandardErrorDTO.java
@@ -0,0 +1,21 @@
+package br.com.main.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StandardErrorDTO implements Serializable{
+   
+    private static final long serialVersionUID = 1L;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+    private String ip;
+    
+}
