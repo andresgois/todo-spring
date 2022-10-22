046f666805de3975bcfdc4d0dbb69eb0cbd6244c
Andre Gois <francisco.gois@pdcase.com.br>
2022-10-22 16:45:16 -0300

    :sparkles: feat: criacao do metodo para buscar por id

--- src/main/java/br/com/main/service/TodoService.java
+++ src/main/java/br/com/main/service/TodoService.java
@@ -5,17 +5,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.main.config.MapperConfig;
+import br.com.main.dto.TodoRequestDTO;
+import br.com.main.mapper.TodoMapper;
 import br.com.main.model.Todo;
 import br.com.main.repository.TodoRepository;
+import br.com.main.service.exception.EntityNotFoundException;
 
 @Service
 public class TodoService {
     
+    private MapperConfig mapper;
     private TodoRepository repository;
     
     @Autowired
-    public TodoService(TodoRepository repository) {
+    public TodoService(TodoRepository repository,MapperConfig mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
     
     public Todo create(Todo todo) {
@@ -24,5 +30,12 @@ public class TodoService {
     
     public List<Todo> all() {
         return repository.findAll();
+    }
+
+    public Todo findTodo(Long id) throws Exception {
+        
+        return repository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Id not foind: "+id));
+        
     }
 }
